aaa60c9d8ed4a7bb11e15d57508fc78822a5371f
Ravinder Jangra <ravinderjangra@live.com>
2019-05-29 11:38:01 +0530

    iOS Updated HybridWebView with safe schema handler

--- SafeMobileBrowser/SafeMobileBrowser.iOS/ControlRenderers/HybridWebViewRenderer.cs
+++ SafeMobileBrowser/SafeMobileBrowser.iOS/ControlRenderers/HybridWebViewRenderer.cs
@@ -1,11 +1,7 @@
 ﻿using Foundation;
 using SafeMobileBrowser.Controls;
 using SafeMobileBrowser.iOS.ControlRenderers;
-using SafeMobileBrowser.WebFetchImplementation;
-using System;
-using System.Text;
-using System.Threading.Tasks;
-using UIKit;
+using System.IO;
 using WebKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -13,90 +9,75 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
 namespace SafeMobileBrowser.iOS.ControlRenderers
 {
-    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, WKWebView>
+    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, WKWebView>, IWKUIDelegate
     {
-        private WKUserContentController _userController;
-        private bool _ignoreSourceChanges;
+        public static string BaseUrl { get; set; } = NSBundle.MainBundle.BundlePath;
+
+        HybridNavigationDelegate _navigationDelegate;
+        WKWebViewConfiguration _configuration;
+        WKUserContentController _contentController;
 
         protected override void OnElementChanged(ElementChangedEventArgs<HybridWebView> e)
         {
             base.OnElementChanged(e);
 
-            if (Control == null && e.NewElement != null)
-            {
-                _userController = new WKUserContentController();
+            if (Control == null && Element != null)
+                SetupControl();
 
-                var config = new WKWebViewConfiguration { UserContentController = _userController };
-                var webView = new WKWebView(Frame, config)
-                {
-                    NavigationDelegate = new CustomWKNavigationDelegate()
-                };
-                SetNativeControl(webView);
-            }
+            if (e.NewElement != null)
+                SetupElement(e.NewElement);
+
+            if (e.OldElement != null)
+                DestroyElement(e.OldElement);
         }
 
-        protected override async void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        void SetupElement(HybridWebView element)
+        {
+            SetSource();
+        }
+
+        private void SetSource()
+        {
+            if (Control == null || Element == null) return;
+
+            var path = Path.Combine(BaseUrl, "startbrowsing.html");
+            var nsFileUri = new NSUrl($"file://{path}");
+            var nsBaseUri = new NSUrl($"file://{BaseUrl}");
+
+            Control.LoadFileUrl(nsFileUri, nsBaseUri);
+        }
+
+        void DestroyElement(HybridWebView element)
+        {
+        }
+
+        void SetupControl()
+        {
+            _navigationDelegate = new HybridNavigationDelegate(this);
+            _contentController = new WKUserContentController();
+
+            _configuration = new WKWebViewConfiguration
+            {
+                UserContentController = _contentController
+            };
+            _configuration.SetUrlSchemeHandler(new SafeSchemaHandler(), "safe");
+
+            var wkWebView = new WKWebView(Frame, _configuration)
+            {
+                Opaque = false,
+                UIDelegate = this,
+                NavigationDelegate = _navigationDelegate
+            };
+
+            SetNativeControl(wkWebView);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             base.OnElementPropertyChanged(sender, e);
             if (e.PropertyName == nameof(Element.Uri))
             {
-                await LoadHtmlFromSafe();
-            }
-        }
-
-        private async Task LoadHtmlFromSafe()
-        {
-            if (_ignoreSourceChanges)
-            {
-                return;
-            }
-
-            var safeResponse = await WebFetchService.FetchResourceAsync(Element.Uri.ToString());
-            var base64String = Convert.ToBase64String(safeResponse.Data);
-            string decodedString = Encoding.UTF8.GetString(safeResponse.Data);
-            Control.LoadHtmlString(decodedString, new NSUrl(Element.Uri));
-            //var imageLinks = LinkFinder.FindImages(decodedString);
-            //if (imageLinks.Count > 0)
-            //{
-            //    foreach (var item in imageLinks)
-            //    {
-            //        var td = await WebFetchService.FetchResourceAsync(Element.Uri + "/" + item.Href.ToString());
-            //        var st = Convert.ToBase64String(td.Data);
-            //        var data = $"{td.MimeType};base64,{st}";
-            //        Control.LoadData(new NSData(data, NSDataBase64DecodingOptions.None),
-            //            td.MimeType,
-            //            "UTF-8", new NSUrl(Element.Uri + "/" + item.Href));
-            //    };
-            //}
-        }
-
-        private class CustomWKNavigationDelegate : WKNavigationDelegate
-        {
-            public override void DecidePolicy(WKWebView webView, WKNavigationAction navigationAction, Action<WKNavigationActionPolicy> decisionHandler)
-            {
-                try
-                {
-                    var policy = WKNavigationActionPolicy.Allow;
-                    if (navigationAction.NavigationType == WKNavigationType.LinkActivated)
-                    {
-                        var url = new NSUrl(navigationAction.Request.Url.ToString());
-                        if (UIApplication.SharedApplication.CanOpenUrl(url))
-                        {
-                            UIApplication.SharedApplication.OpenUrl(url);
-                            policy = WKNavigationActionPolicy.Cancel;
-                        }
-                    }
-                    decisionHandler?.Invoke(policy);
-                }
-                catch
-                {
-                    base.DecidePolicy(webView, navigationAction, decisionHandler);
-                }
-            }
-
-            public override void DidReceiveAuthenticationChallenge(WKWebView webView, NSUrlAuthenticationChallenge challenge, Action<NSUrlSessionAuthChallengeDisposition, NSUrlCredential> completionHandler)
-            {
-                completionHandler?.Invoke(NSUrlSessionAuthChallengeDisposition.PerformDefaultHandling, null);
+                Control.LoadRequest(NSUrlRequest.FromUrl(NSUrl.FromString(Element.Uri)));
             }
         }
     }
---
+++ SafeMobileBrowser/SafeMobileBrowser.iOS/ControlRenderers/SafeSchemaHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Foundation;
+using SafeMobileBrowser.WebFetchImplementation;
+using WebKit;
+
+namespace SafeMobileBrowser.iOS.ControlRenderers
+{
+    public class SafeSchemaHandler : NSObject, IWKUrlSchemeHandler
+    {
+        [Export("webView:startURLSchemeTask:")]
+        public void StartUrlSchemeTask(WKWebView webView, IWKUrlSchemeTask urlSchemeTask)
+        {
+            Task.Run(async () =>
+            {
+                var saferesponse = await WebFetchService.FetchResourceAsync(urlSchemeTask.Request.Url.ToString());
+                Stream stream = new MemoryStream(saferesponse.Data);
+                var response = new NSUrlResponse(urlSchemeTask.Request.Url, saferesponse.MimeType, (nint)stream.Length, null);
+                urlSchemeTask.DidReceiveResponse(response);
+                urlSchemeTask.DidReceiveData(NSData.FromStream(stream));
+                urlSchemeTask.DidFinish();
+            }).Wait();
+        }
+
+        [Export("webView:stopURLSchemeTask:")]
+        public void StopUrlSchemeTask(WKWebView webView, IWKUrlSchemeTask urlSchemeTask)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
