3fcaad7e9a36281710ddcc7e3bd9b9ff02c8e551
Ravinder Jangra <ravinderjangra@live.com>
2020-01-22 14:13:10 +0530

    chore(build): add get_release_desc file to generate release content

---
+++ CakeScripts/get_release_description.sh
@@ -0,0 +1,30 @@
+read -r -d '' release_description << 'EOF'
+The SAFE Mobile Browser is a cross-platform mobile (Android, iOS) browser, built to provide web browsing experience to the users on the SAFE Network.
+
+## Changelog
+CHANGELOG_CONTENT
+
+## SHA-256 checksums:
+```
+APK checksum
+APK_CHECKSUM
+
+IPA checksum
+IPA_CHECKSUM
+```
+
+## Related Links
+* [SAFE Mobile Authenticator Browser](https://github.com/maidsafe/safe-authenticator-mobile/releases/)
+* [SAFE Desktop Browser](https://github.com/maidsafe/safe_browser/releases/)
+* [SAFE CLI](https://github.com/maidsafe/safe-api/tree/master/safe-cli)
+* [SAFE Vault](https://github.com/maidsafe/safe_vault/releases/latest/)
+* [safe_app_csharp](https://github.com/maidsafe/safe_app_csharp/)
+EOF
+
+apk_checksum=$(sha256sum "../net.maidsafe.browser.apk" | awk '{ print $1 }')
+ipa_checksum=$(sha256sum "../SafeMobileBrowser.iOS.ipa" | awk '{ print $1 }')
+changelog_content=$(sed '1,/)/d;/##/,$d' ../CHANGELOG.MD)
+release_description=$(sed "s/APK_CHECKSUM/$apk_checksum/g" <<< "$release_description")
+release_description=$(sed "s/IPA_CHECKSUM/$ipa_checksum/g" <<< "$release_description")
+
+echo "${release_description/CHANGELOG_CONTENT/$changelog_content}" > release_description.txt
